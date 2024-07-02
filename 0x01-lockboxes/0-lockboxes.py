49c3a1ffdb5b356df7f622ebf13045ea0d3ef546
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-02 17:19:25 +0100

    Added docstrings to module and functions; changed `is` to `==` in conditional statement.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""Module is fucking documented"""
 _boxes = []
 
 
 def canUnlockAll(boxes):
+    """Shit is fucking documented"""
     global _boxes
     _boxes = boxes
     num_of_boxes = len(boxes)
     global visited
     visited = [False] * num_of_boxes
     visit(boxes[0], 0)
-    if visited is ([True] * num_of_boxes):
+    if visited == ([True] * num_of_boxes):
         return True
     return False
 
 
 def visit(box, index):
+    """Shit is fucking documented"""
     if visited[index] is True:
         return
     visited[index] = True
