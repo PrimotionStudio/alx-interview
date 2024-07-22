32fab9170473e6409b1bced5ae94f13418e271eb
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-23 00:34:54 +0100

    "Added type hints and implemented minOperations function"

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,6 +4,16 @@ This module contains a function that
 returns the minimum number of operation
 required to copy and paste a character in a text editor
 """
+from typing import List, Tuple
+
 
 def minOperations(n: int) -> int:
-    pass
+    arr: List[Tuple[int]] = []
+    i: int = 2
+    while i < n / 2:
+        if i % n == 0:
+            arr.append((i, n).abs(i - (n / i)))
+    if len(arr) == 0:
+        return n
+    sorted(arr)
+    return arr[0][0] + arr[0][1]
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+method that determines if a given data
+set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    method that determines if a given data
+    set represents a valid UTF-8 encoding
+    """
+    pass
