968c10a0d077860e586a0dfa6246219d1c185e55
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-23 02:25:47 +0100

    "Removed print statements from UTF-8 validation function"

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,27 +23,27 @@ def validUTF8(data):
     _bin = [bin(i)[2:].zfill(8) for i in data]
     i = 0
     while i < len(_bin):
-        print("i is", i)
+        # print("i is", i)
         # while iterating, `i` must not land on bytes
         # starting with `10` else, it will be considered
         # an error because provisions will be made for bytes
         # starting with `10` when their headding bytes are found
         byte = _bin[i]
-        print(byte[0:4])
+        # print(byte[0:4])
         if byte[0] == '0':
             # Matches with ASCII
-            print("Matches with ASCII")
+            # print("Matches with ASCII")
             rest = byte[1:] # correct ASCII or UTF-8 code
             # pass
         elif byte[0:2] == '10':
             # 2nd, 3rd or 4th byte of the immediate below 3
-            print("Not a leading byte, AN ERROR, SKIP!")
+            # print("Not a leading byte, AN ERROR, SKIP!")
             isValid = False
             # (110, 1110, 11110)
             # pass
         elif byte[0:3] == '110':
             # Contains 2 bytes code points
-            print("Contains 2 bytes code points")
+            # print("Contains 2 bytes code points")
             # Looping through the 2 bytes and taking
             # the code points and removing its flags
             j = 0
@@ -51,7 +51,7 @@ def validUTF8(data):
             code_points = ''
             while i < 2:
                 _byte = _bin[i]
-                print("Viewing following bytes", _byte[0:4])
+                # print("Viewing following bytes", _byte[0:4])
                 # Get the heading bytes and remove its flag
                 if j == 0:
                     # heading flag
@@ -65,7 +65,7 @@ def validUTF8(data):
                         # the following bytes were not consistent
                         # with `10` and is generating an error
                         err = 1 # set err flag to 1
-                        print("the following bytes were not consistent")
+                        # print("the following bytes were not consistent")
                         isValid = False
                         break
                 j = 1
@@ -73,14 +73,14 @@ def validUTF8(data):
             if err == 0:
                 # if err flag is not set, then the code points
                 # are correct
-                print("Correct code points")
+                pass
             i += 1
-            print("sub i is", i)
+            # print("sub i is", i)
             j = 0 # reset j just in case
             # pass
         elif byte[0:4] == '1110':
             # Contains 3 bytes code points
-            print("Contains 3 bytes code points")
+            # print("Contains 3 bytes code points")
             # Looping through the 3 bytes and taking
             # the code points and removing its flags
             j = 0
@@ -88,7 +88,7 @@ def validUTF8(data):
             code_points = ''
             while i < 3:
                 _byte = _bin[i]
-                print("Viewing following bytes", _byte[0:4])
+                # print("Viewing following bytes", _byte[0:4])
                 # Get the heading bytes and remove its flag
                 if j == 0:
                     # heading flag
@@ -102,7 +102,7 @@ def validUTF8(data):
                         # the following bytes were not consistent
                         # with `10` and is generating an error
                         err = 1 # set err flag to 1
-                        print("the following bytes were not consistent")
+                        # print("the following bytes were not consistent")
                         isValid = False
                         break
                 j = 1
@@ -110,14 +110,15 @@ def validUTF8(data):
             if err == 0:
                 # if err flag is not set, then the code points
                 # are correct
-                print("Correct code points")
+                # print("Correct code points")
+                pass
             i += 2
-            print("sub i is", i)
+            # print("sub i is", i)
             j = 0 # reset j just in case
             # pass
         elif byte[0:5] == '11110':
             # Contains 4 bytes code points
-            print("Contains 4 bytes code points")
+            # print("Contains 4 bytes code points")
             # Looping through the 4 bytes and taking
             # the code points and removing its flags
             j = 0
@@ -125,7 +126,7 @@ def validUTF8(data):
             code_points = ''
             while i < 4:
                 _byte = _bin[i]
-                print("Viewing following bytes", _byte[0:4])
+                # print("Viewing following bytes", _byte[0:4])
                 # Get the heading bytes and remove its flag
                 if j == 0:
                     # heading flag
@@ -139,7 +140,7 @@ def validUTF8(data):
                         # the following bytes were not consistent
                         # with `10` and is generating an error
                         err = 1 # set err flag to 1
-                        print("the following bytes were not consistent")
+                        # print("the following bytes were not consistent")
                         isValid = False
                         break
                 j = 1
@@ -147,14 +148,15 @@ def validUTF8(data):
             if err == 0:
                 # if err flag is not set, then the code points
                 # are correct
-                print("Correct code points")
+                # print("Correct code points")
+                pass
             i += 3
-            print("sub i is", i)
+            # print("sub i is", i)
             j = 0 # reset j just in case
             # pass
         else:
             # Invalid
             pass
         i += 1
-    print(_bin)
+    # print(_bin)
     return isValid
