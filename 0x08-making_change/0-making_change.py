812745187d279882b82ec53208aa852984a9cd7b
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-23 11:04:53 +0100

    "Refactor makeChange function to use iterative approach instead of recursive, remove unnecessary code and variables"

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
 """
-a solution to the making change problem
+A more optimal solution to the making change problem.
 """
-import sys
-
-
-change = []
-
 
 def makeChange(coins, total):
-    """a function to make change
-    first make sure list is sorted to
-    ensure the biggest value is at the end
     """
-    sys.setrecursionlimit(9999999)
+    Function to determine the minimum number
+    of coins needed to make change.
+    """
     if total <= 0:
         return 0
-    if not any([True for x in coins if x <= total]):
-        return -1
-    coins.sort()
-    t = recursive(coins, total)
-    if t == 0:
-        return len(change)
-    else:
-        return -1
-
-
-def recursive(coins, total):
-    """recursion to make change"""
-    if total == 0:
-        return total
-    if total <= 0:
-        return -1
-    # trim coins if necessary
-    coins = [x for x in coins if x <= total]
-    if len(coins) == 0:
-        return -1
-    total -= coins[-1]
-    change.append(coins[-1])
-    return recursive(coins, total)
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        if coin <= total:
+            count += total // coin
+            total %= coin
+    return count if total == 0 else -1
