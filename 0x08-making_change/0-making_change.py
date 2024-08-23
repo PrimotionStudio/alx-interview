76648cecd011486c7fafbac2637d0b820d8e0a76
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-23 11:01:01 +0100

    Added import statement, increased recursion limit, and minor whitespace changes to making change problem solution.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,14 +2,18 @@
 """
 a solution to the making change problem
 """
+import sys
+
 
 change = []
+
 
 def makeChange(coins, total):
     """a function to make change
     first make sure list is sorted to
     ensure the biggest value is at the end
     """
+    sys.setrecursionlimit(9999999)
     if total <= 0:
         return 0
     if not any([True for x in coins if x <= total]):
@@ -20,6 +24,7 @@ def makeChange(coins, total):
         return len(change)
     else:
         return -1
+
 
 def recursive(coins, total):
     """recursion to make change"""
