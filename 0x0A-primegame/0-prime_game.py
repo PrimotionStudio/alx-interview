e00dbf0851090b3544e337cc18e6a96b2d9bbcad
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-06 13:19:50 +0100

    "Refactored prime game implementation, removed precomputation of Grundy numbers, and updated win condition logic."

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
-"""Precompute primes and Grundy numbers"""
+"""Game of choosing Prime numbers"""
 
 
 def isWinner(x, nums):
-    MAX_N = 10000
-    is_prime = [True] * (MAX_N + 1)
-    primes = []
+    max_num = max(nums)
 
-    is_prime[0] = is_prime[1] = False
-    for num in range(2, MAX_N + 1):
-        if is_prime[num]:
-            primes.append(num)
-            for multiple in range(num * num, MAX_N + 1, num):
-                is_prime[multiple] = False
+    sieve = [True] * (max_num + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if sieve[i]:
+            for multiple in range(i * i, max_num + 1, i):
+                sieve[multiple] = False
 
-    grundy = [0] * (MAX_N + 1)
-
-    for n in range(2, MAX_N + 1):
-        reachable = set()
-        for prime in primes:
-            if prime > n:
-                break
-            for multiple in range(prime, n + 1, prime):
-                reachable.add(grundy[n - multiple])
-        # Calculate the minimum excludant (mex)
-        mex = 0
-        while mex in reachable:
-            mex += 1
-        grundy[n] = mex
-
+    primes_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        primes_count[i] = primes_count[i - 1] + (1 if sieve[i] else 0)
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if grundy[n] != 0:
-            maria_wins += 1
-        else:
+        if primes_count[n] % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
